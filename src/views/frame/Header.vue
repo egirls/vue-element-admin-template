2f6c3a6d8c2a66b4c00ce3368ec08abb98d557b3
y.liu@herin.ai <y.liu@herin.ai>
2019-04-08 18:33:50 +0800

    add log out

--- src/views/frame/Header.vue
+++ src/views/frame/Header.vue
@@ -1,19 +1,27 @@
 <template>
-  <el-menu :default-active="this.$route.path" router class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" @select="handleSelect">
+  <el-menu :default-active="this.$route.path" router class="el-menu-header" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" @select="handleSelect">
     <el-menu-item index="/">处理中心</el-menu-item>
     <el-submenu index="2">
       <template slot="title">我的工作台</template>
       <el-submenu index="2-1">
         <template slot="title">选项1</template>
-        <el-menu-item index="2-1-1">选项1</el-menu-item>
-        <el-menu-item index="2-1-2">选项2</el-menu-item>
-        <el-menu-item index="2-1-3">选项3</el-menu-item>
+        <el-menu-item>选项1</el-menu-item>
+        <el-menu-item>选项2</el-menu-item>
+        <el-menu-item>选项3</el-menu-item>
       </el-submenu>
       <el-menu-item index="2-2">选项2</el-menu-item>
       <el-menu-item index="2-3">选项3</el-menu-item>
     </el-submenu>
     <el-menu-item index="3">消息中心</el-menu-item>
     <el-menu-item index="4">订单管理</el-menu-item>
+    <div class="right-menu">
+      <el-submenu>
+        <template slot="title">name</template>
+        <el-menu-item>选项1</el-menu-item>
+        <el-menu-item>选项2</el-menu-item>
+        <el-menu-item index="login">Log out</el-menu-item>
+      </el-submenu>
+    </div>
   </el-menu>
 </template>
 
@@ -37,5 +45,11 @@ export default {
 <style>
 li a {
   text-decoration: none;
+}
+
+.right-menu {
+  color: #fff;
+  float: right;
+  line-height: 60px;
 }
 </style>
